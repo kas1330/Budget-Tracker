02e16219ed5a650f8b8db1fdb08bcfeabdbfc9b1
Kerri Sexton <k.sexton804@gmail.com>
2021-01-24 14:11:34 -0500

    env variable in server file and package lock

--- server.js
+++ server.js
@@ -3,21 +3,25 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+require("dotenv").config();
 
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(compression());
+
+//parse data comng through the port, decrypyt it. also defines req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//allows access to the public folder on the client side.
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGO_URI_NEW || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
